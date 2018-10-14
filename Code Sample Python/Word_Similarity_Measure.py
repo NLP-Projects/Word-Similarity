b0e2aec15f4e2656d6565c5d65ba2cb7254b1cc2
NLP-Projects <44062158+NLP-Projects@users.noreply.github.com>
2018-10-14 23:35:03 +0700

    Add files via upload

---
+++ Code Sample Python/Word_Similarity_Measure.py
@@ -0,0 +1,12 @@
+import  io
+import numpy as np
+from scipy import spatial
+rs=[]
+for i in dataset:
+    s=i.split()
+    u1 = s[0].strip() # word 1
+    u2 = s[1].strip() # word 2
+    sim = (2 - spatial.distance.cosine(v1, v2))/2
+    rs.append(k)
+
+ 
